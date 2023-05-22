0a8080078683a540a2eb0f890056c72f384ff21e
leandro <leandro_navarro@outlook.es>
2023-05-21 22:15:59 -0300

    punto 2 de react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+import React from 'react';
+import Saludo from './components/Saludo';
+
+const App = () => {
+  return (
+    <Saludo></Saludo>
+    
+  );
+};
+
+export default App;
